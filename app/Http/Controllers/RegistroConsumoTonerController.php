d5d97db510470e4328f945ff3b737c8db312fe5b
David <drh697@gmail.com>
2019-06-14 09:43:37 -0500

    registro de consumo de toner

---
+++ app/Http/Controllers/RegistroConsumoTonerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\RegistroConsumoToner;
+use Illuminate\Http\Request;
+
+class RegistroConsumoTonerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\RegistroConsumoToner  $registroConsumoToner
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RegistroConsumoToner $registroConsumoToner)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\RegistroConsumoToner  $registroConsumoToner
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RegistroConsumoToner $registroConsumoToner)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\RegistroConsumoToner  $registroConsumoToner
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RegistroConsumoToner $registroConsumoToner)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\RegistroConsumoToner  $registroConsumoToner
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RegistroConsumoToner $registroConsumoToner)
+    {
+        //
+    }
+}
---
+++ app/RegistroConsumoToner.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RegistroConsumoToner extends Model
+{
+    //
+}
